04904a15569ef5127b4c52a19bac74f5802ef26b
Robert Bjornson <rbjornson@gmail.com>
2016-12-15 17:41:28 -0500

    minor change to file name

--- dSQ.py
+++ dSQ.py
@@ -7,7 +7,7 @@ slurmflags=sys.argv[2:]
 
 tasks=len(open(taskfile).readlines())
 
-cmd="sbatch --array=0-%d %s MSQ.py %s" % (tasks-1, " ".join(slurmflags), taskfile)
+cmd="sbatch --array=0-%d %s dSQbatch.py %s" % (tasks-1, " ".join(slurmflags), taskfile)
 
 subprocess.call(cmd, shell=True)
 
