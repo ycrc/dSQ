b6a6671a76a5b2c5784079c033b4b6e7ff440a8c
Robert Bjornson <rbjornson@gmail.com>
2016-12-15 17:59:54 -0500

    added code to let dSQ.py find dSQbatch.py

--- dSQ.py
+++ dSQ.py
@@ -1,13 +1,14 @@
 #!/bin/env python
 
-import sys, subprocess
+import sys, subprocess, os
 
 taskfile=sys.argv[1]
 slurmflags=sys.argv[2:]
 
+script=os.path.join(os.path.dirname(os.path.abspath(sys.argv[0])), 'dSQbatch.py')
 tasks=len(open(taskfile).readlines())
 
-cmd="sbatch --array=0-%d %s dSQbatch.py %s" % (tasks-1, " ".join(slurmflags), taskfile)
+cmd="sbatch --array=0-%d %s %s %s" % (tasks-1, " ".join(slurmflags), script, taskfile)
 
 subprocess.call(cmd, shell=True)
 
