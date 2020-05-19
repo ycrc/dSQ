9295a0afb1ff637b8d12d200d35b0baf337457b3
Ben Evans <benjamin.robert.evans@gmail.com>
2020-05-19 12:07:43 -0400

    less bad deploy script

--- .ycrc/update_dsq_module.sh
+++ .ycrc/update_dsq_module.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
-shopt -s expand_aliases
+#set -o xtrace
 source /etc/yalehpc
-DSQ_CV=1.03
-DSQ_NV=$( echo "${DSQ_CV} + 0.01" | bc )
+DSQ_CV=$( module load dSQ && dsq --version 2>&1 | cut -d" " -f2 )
+DSQ_NV=$( curl --silent "https://api.github.com/repos/ycrc/dSQ/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/' )
 
-
-module load EasyBuild && \
-      eb dSQ-${DSQ_CV}.eb --try-software-version=${DSQ_NV} && \
-      cd ~/repos/ycrc-ebfiles && \
+if [ ! $DSQ_CV == $DSQ_NV ]
+then
+    cd ~/repos/ycrc-ebfiles && \
       git pull  && \
+      module load EasyBuild && \
+      eb dSQ-${DSQ_CV}.eb --try-software-version=${DSQ_NV} && \
       git add easyconfigs/dSQ/dSQ-${DSQ_NV}.eb && \
       git commit -m "installed dSQ v${DSQ_NV} on ${cluster}" && \
       git push
-
+else
+    echo "dSQ is up to date!"
+fi
